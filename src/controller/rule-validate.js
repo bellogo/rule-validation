6321a2a1b9e79b8babb0a23563534391056260fa
Ufuoma Ogodo <ufuomaogodo@gmail.com>
2021-03-11 16:27:07 +0100

    add changes

--- src/controller/rule-validate.js
+++ src/controller/rule-validate.js
@@ -1,43 +1,62 @@
 export default class ruleController {
   static homePage(req, res) {
-    res.status(200).json({
-      message: "My Rule-Validation API",
-      status: "success",
-      data: {
-        name: "Ogodo Ufuoma Bella",
-        github: "@bellogo",
-        email: "ufuomaogodo@gmail.com",
-        mobile: "08165341913",
-        twitter: "@dcodeknight",
-      },
-    });
+    try {
+      return res.status(200).json({
+        message: "My Rule-Validation API",
+        status: "success",
+        data: {
+          name: "Ogodo Ufuoma Bella",
+          github: "@bellogo",
+          email: "ufuomaogodo@gmail.com",
+          mobile: "08165341913",
+          twitter: "@dcodeknight",
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({ message: "Server Error.", status: 500, data: null });
+    }
   }
 
   static validateRule(req, res) {
-    const { condition, condition_value, field } = req.body.rule;
-    const { field_value } = req.body;
-    const isValid = () => {
-      switch (condition) {
-        case "eq":
-          return field_value === condition_value;
-        case "neq":
-          return field_value !== condition_value;
-        case "gt":
-          return field_value > condition_value;
-        case "gte":
-          return field_value >= condition_value;
-        case "contains":
-          return field_value === condition_value;
-      }
-    };
+    try {
+      const { condition, condition_value, field } = req.body.rule;
+      const { field_value } = req.body;
+      const isValid = () => {
+        switch (condition) {
+          case "eq":
+            return field_value === condition_value;
+          case "neq":
+            return field_value !== condition_value;
+          case "gt":
+            return field_value > condition_value;
+          case "gte":
+            return field_value >= condition_value;
+          case "contains":
+            return field_value === condition_value;
+        }
+      };
 
-    if (isValid()) {
+      if (isValid()) {
+        return res.status(200).json({
+          message: `field ${field} successfully validated.`,
+          status: "success",
+          data: {
+            validation: {
+              error: false,
+              field,
+              field_value,
+              condition,
+              condition_value
+            }
+          }
+        });
+      }
       return res.status(200).json({
-        message: `field ${field} successfully validated.`,
-        status: "success",
+        message: `field ${field} failed validation.`,
+        status: "error",
         data: {
           validation: {
-            error: false,
+            error: true,
             field,
             field_value,
             condition,
@@ -45,19 +64,8 @@ export default class ruleController {
           }
         }
       });
+    } catch (error) {
+      return res.status(500).json({ message: "Server Error.", status: 500, data: null });
     }
-    return res.status(200).json({
-      message: `field ${field} failed validation.`,
-      status: "error",
-      data: {
-        validation: {
-          error: true,
-          field,
-          field_value,
-          condition,
-          condition_value
-        }
-      }
-    });
   }
 }
--- src/middleware/helpers.js
+++ src/middleware/helpers.js
@@ -2,23 +2,27 @@ import joiValidate from "../validation/joiValidation";
 
 export default class helpers {
   static errorCheck(req, res, next) {
-    const { error } = joiValidate(req.body);
-    if (error) {
-      return res.status(400).json({ message: error.message, status: "error", data: null });
-    }
+    try {
+      const { error } = joiValidate(req.body);
+      if (error) {
+        return res.status(400).json({ message: error.message, status: "error", data: null });
+      }
 
-    const { data } = req.body;
-    const { field } = req.body.rule;
-    const fieldArray = field.split(".");
+      const { data } = req.body;
+      const { field } = req.body.rule;
+      const fieldArray = field.split(".");
 
-    if (fieldArray.length === 1 && data[fieldArray[0]]) {
-      req.body.field_value = data[fieldArray[0]];
-      return next();
+      if (fieldArray.length === 1 && data[fieldArray[0]]) {
+        req.body.field_value = data[fieldArray[0]];
+        return next();
+      }
+      if (fieldArray.length === 2 && data[fieldArray[0]] && data[fieldArray[0]][fieldArray[1]]) {
+        req.body.field_value = data[fieldArray[0]][fieldArray[1]];
+        return next();
+      }
+      return res.status(400).json({ message: `field ${field} is missing from data.`, status: "error", data: null });
+    } catch (err) {
+      return res.status(500).json({ message: "Server Error.", status: 500, data: null });
     }
-    if (fieldArray.length === 2 && data[fieldArray[0]] && data[fieldArray[0]][fieldArray[1]]) {
-      req.body.field_value = data[fieldArray[0]][fieldArray[1]];
-      return next();
-    }
-    return res.status(400).json({ message: `field ${field} is missing from data.`, status: "error", data: null });
   }
 }
